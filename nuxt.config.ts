923fe13e08579c408c84b6b7ff940035fd38713c
programando <jhonjamesmg@hotmail.com>
2022-10-21 05:55:06 -0500

    add pinia statement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: [ //instead of buildModules
+  '@pinia/nuxt'
+  ],
     build: {
         postcss: {
           postcssOptions: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="container mx-auto bg-gray-600 p-8 font-Poppins">
-      <h1 class=" text-yellow-100 text-lg">ESTE ES UN EJEMPLO USANDO OPTION API, PERO USAREMOS COMPOSITION API EN TODO EL PROYECTO</h1>
- 
+      <h1 class=" text-yellow-100 text-lg">
+        ESTE ES UN EJEMPLO USANDO OPTION API, PERO USAREMOS COMPOSITION API EN TODO EL PROYECTO
+      <br>Count : {{ store.count}} 
+      </h1>
+      
+      <button class="text-white" @click="StateCahnge()"> Cambiar estado</button>
     </div>
+   
   </template>
 
-  <script setup>
+ <!-- -->
+  <script setup >
+  import { useCounterStore } from "@/stores/counter";
+
+ 
+  const store =  useCounterStore ()  ;
+ 
+
   // ESTE ES UN EJEMPLO USANDO OPTION API
-  import Clientes from  "@/api/Terceros"
-   
-    
-    onMounted(async  () => {
+  //import Clientes from  "@/api/Terceros"
+  
+  onMounted(async  () => {
       //const response= await Clientes.exampleRoute('foflf');
       //console.log ( response )
         //const AppConfig = useRuntimeConfig()
         //console.log (AppConfig.urlServerApi)
-        
-     
   })
+
+    const StateCahnge = () => {
+        store.increment( 4);
+    }
+
   </script>
 
 
